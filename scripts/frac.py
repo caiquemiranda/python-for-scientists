cb0314d1b42c9869806d69644e0c43adebc085dd
caiquemiranda <caiquemirandas@gmail.com>
2023-04-04 06:39:46 -0300

    frac

--- scripts/frac.py
+++ scripts/frac.py
@@ -3,39 +3,39 @@
 import gcd
 
 class Frac:
-""" Fractional class. A Frac is a pair of integers num, den
-(with den!=0) whose GCD is 1.
-"""
-
-def __init__(self,n,d):
-    """ Construct a Frac from integers n and d.
-    Needs error message if d = 0!
+    """ Fractional class. A Frac is a pair of integers num, den
+    (with den!=0) whose GCD is 1.
     """
 
-    hcf = gcd.gcd(n, d)
-    self.num, self.den = n/hcf, d/hcf
+    def __init__(self,n,d):
+        """ Construct a Frac from integers n and d.
+        Needs error message if d = 0!
+        """
 
-def __str__(self):
-    """ Generate a string representation of a Frac. """
+        hcf = gcd.gcd(n, d)
+        self.num, self.den = n/hcf, d/hcf
 
-    return "%d/%d" % (self.num, self.den)
+    def __str__(self):
+        """ Generate a string representation of a Frac. """
 
-def __mul__(self,another):
-    """ Multiply two Fracs to produce a Frac. """
+        return "%d/%d" % (self.num, self.den)
 
-    return Frac(self.num * another.num, 
-                self.den * another.den)
+    def __mul__(self,another):
+        """ Multiply two Fracs to produce a Frac. """
 
-def __add__(self,another):
-    """ Add two Fracs to produce a Frac. """
+        return Frac(self.num * another.num, 
+                    self.den * another.den)
 
-    return Frac(self.num * another.den + self.den * another.num, 
-                self.den * another.den)
+    def __add__(self,another):
+        """ Add two Fracs to produce a Frac. """
 
-def to_real(self):
-    """ return floating point value of Frac. """
+        return Frac(self.num * another.den + self.den * another.num, 
+                    self.den * another.den)
 
-    return float(self.num)/float(self.den)
+    def to_real(self):
+        """ return floating point value of Frac. """
+
+        return float(self.num)/float(self.den)
 
 if __name__=="__main__":
     a=Frac(3,7)
